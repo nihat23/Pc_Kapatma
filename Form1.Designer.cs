9dd267a294223f1d428bc84e42efe1571369cc6c
nihat23 <62906442+nihat23@users.noreply.github.com>
2021-04-30 16:48:23 +0300

    Pc Kapatma

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Pc_Kapatma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1Kapat = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button2Yeniden = new System.Windows.Forms.Button();
+            this.button3Kullanici = new System.Windows.Forms.Button();
+            this.button4Uyku = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1Kapat
+            // 
+            this.button1Kapat.BackColor = System.Drawing.Color.White;
+            this.button1Kapat.Font = new System.Drawing.Font("Lucida Bright", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1Kapat.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1Kapat.ImageIndex = 1;
+            this.button1Kapat.ImageList = this.ımageList1;
+            this.button1Kapat.Location = new System.Drawing.Point(3, 2);
+            this.button1Kapat.Name = "button1Kapat";
+            this.button1Kapat.Size = new System.Drawing.Size(158, 161);
+            this.button1Kapat.TabIndex = 0;
+            this.button1Kapat.Text = "Bilgisayar Kapat";
+            this.button1Kapat.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1Kapat.UseVisualStyleBackColor = false;
+            this.button1Kapat.Click += new System.EventHandler(this.button1Kapat_Click);
+            this.button1Kapat.MouseLeave += new System.EventHandler(this.button1Kapat_MouseLeave);
+            this.button1Kapat.MouseHover += new System.EventHandler(this.button1Kapat_MouseHover);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "kullanici.png");
+            this.ımageList1.Images.SetKeyName(1, "unnamed.jpg");
+            this.ımageList1.Images.SetKeyName(2, "eeeee.png");
+            this.ımageList1.Images.SetKeyName(3, "istockphoto-1141264287-170667a.jpg");
+            // 
+            // button2Yeniden
+            // 
+            this.button2Yeniden.BackColor = System.Drawing.Color.White;
+            this.button2Yeniden.Font = new System.Drawing.Font("Lucida Bright", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2Yeniden.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2Yeniden.ImageIndex = 2;
+            this.button2Yeniden.ImageList = this.ımageList1;
+            this.button2Yeniden.Location = new System.Drawing.Point(167, 2);
+            this.button2Yeniden.Name = "button2Yeniden";
+            this.button2Yeniden.Size = new System.Drawing.Size(152, 161);
+            this.button2Yeniden.TabIndex = 1;
+            this.button2Yeniden.Text = "Yeniden başlat";
+            this.button2Yeniden.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2Yeniden.UseVisualStyleBackColor = false;
+            this.button2Yeniden.Click += new System.EventHandler(this.button2Yeniden_Click);
+            this.button2Yeniden.MouseLeave += new System.EventHandler(this.button2Yeniden_MouseLeave);
+            this.button2Yeniden.MouseHover += new System.EventHandler(this.button2Yeniden_MouseHover);
+            // 
+            // button3Kullanici
+            // 
+            this.button3Kullanici.BackColor = System.Drawing.Color.White;
+            this.button3Kullanici.Font = new System.Drawing.Font("Lucida Bright", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3Kullanici.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3Kullanici.ImageIndex = 0;
+            this.button3Kullanici.ImageList = this.ımageList1;
+            this.button3Kullanici.Location = new System.Drawing.Point(348, 2);
+            this.button3Kullanici.Name = "button3Kullanici";
+            this.button3Kullanici.Size = new System.Drawing.Size(150, 161);
+            this.button3Kullanici.TabIndex = 2;
+            this.button3Kullanici.Text = "Kullanıcı Degiştir";
+            this.button3Kullanici.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3Kullanici.UseVisualStyleBackColor = false;
+            this.button3Kullanici.Click += new System.EventHandler(this.button3Kullanici_Click);
+            this.button3Kullanici.MouseLeave += new System.EventHandler(this.button3Kullanici_MouseLeave);
+            this.button3Kullanici.MouseHover += new System.EventHandler(this.button3Kullanici_MouseHover);
+            // 
+            // button4Uyku
+            // 
+            this.button4Uyku.BackColor = System.Drawing.Color.White;
+            this.button4Uyku.Font = new System.Drawing.Font("Lucida Bright", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4Uyku.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4Uyku.ImageIndex = 3;
+            this.button4Uyku.ImageList = this.ımageList1;
+            this.button4Uyku.Location = new System.Drawing.Point(516, 2);
+            this.button4Uyku.Name = "button4Uyku";
+            this.button4Uyku.Size = new System.Drawing.Size(150, 161);
+            this.button4Uyku.TabIndex = 3;
+            this.button4Uyku.Text = "Bilgisayar Uyku modu ";
+            this.button4Uyku.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4Uyku.UseVisualStyleBackColor = false;
+            this.button4Uyku.Click += new System.EventHandler(this.button4Uyku_Click);
+            this.button4Uyku.MouseLeave += new System.EventHandler(this.button4Uyku_MouseLeave);
+            this.button4Uyku.MouseHover += new System.EventHandler(this.button4Uyku_MouseHover);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(611, 166);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Nihat Beyi";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(679, 182);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button4Uyku);
+            this.Controls.Add(this.button3Kullanici);
+            this.Controls.Add(this.button2Yeniden);
+            this.Controls.Add(this.button1Kapat);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pc işlemleri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1Kapat;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Button button2Yeniden;
+        private System.Windows.Forms.Button button3Kullanici;
+        private System.Windows.Forms.Button button4Uyku;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pc_Kapatma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2Yeniden_Click(object sender, EventArgs e)
+        {
+            DialogResult soru = MessageBox.Show("Bilgisayarı Yeniden Başlatmak istediginizden eminmisiniz..", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (soru == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "-r -f");
+            }
+        }
+
+        private void button1Kapat_Click(object sender, EventArgs e)
+        {
+            DialogResult soru = MessageBox.Show("Bilgisayarı Kapatmak istediginizden eminmisiniz..", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (soru == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "-f -s");
+            }
+        }
+
+        private void button3Kullanici_Click(object sender, EventArgs e)
+        {
+            DialogResult soru = MessageBox.Show("Kullanıcı Adı degiştirmesine eminmisiniz..", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (soru == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "-l");
+            }
+        }
+
+        private void button4Uyku_Click(object sender, EventArgs e)
+        {
+            DialogResult soru = MessageBox.Show("Uyku moduna geçiyor..", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (soru == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "-h");
+            }
+        }
+
+        private void button1Kapat_MouseHover(object sender, EventArgs e)
+        {
+            button1Kapat.BackColor = Color.Red;
+        }
+
+        private void button1Kapat_MouseLeave(object sender, EventArgs e)
+        {
+            button1Kapat.BackColor = Color.White;
+        }
+
+        private void button2Yeniden_MouseHover(object sender, EventArgs e)
+        {
+            button2Yeniden.BackColor = Color.Red;
+        }
+
+        private void button2Yeniden_MouseLeave(object sender, EventArgs e)
+        {
+            button2Yeniden.BackColor = Color.White;
+        }
+
+        private void button3Kullanici_MouseHover(object sender, EventArgs e)
+        {
+            button3Kullanici.BackColor = Color.Red;
+        }
+
+        private void button3Kullanici_MouseLeave(object sender, EventArgs e)
+        {
+            button3Kullanici.BackColor = Color.White;
+        }
+
+        private void button4Uyku_MouseHover(object sender, EventArgs e)
+        {
+            button4Uyku.BackColor = Color.Red;
+        }
+
+        private void button4Uyku_MouseLeave(object sender, EventArgs e)
+        {
+            button4Uyku.BackColor = Color.White;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/n.beyi");
+        }
+    }
+}
